34fa080bdaab4bef34b1a57b6d2ed354d9d11ecd
Ruediger Birkner <ruediger.birkner@dfinity.org>
2024-07-09 15:27:37 +0000

    make raw never verify the certificate

--- packages/ic-http-gateway/src/protocol/validate.rs
+++ packages/ic-http-gateway/src/protocol/validate.rs
@@ -1,4 +1,4 @@
-use crate::{HttpGatewayResult, IC_CERTIFICATE_HEADER_NAME};
+use crate::HttpGatewayResult;
 use candid::Principal;
 use ic_agent::Agent;
 use ic_http_certification::{HttpRequest, HttpResponse};
@@ -16,35 +16,24 @@ pub fn validate(
     response: HttpResponse,
     allow_skip_verification: bool,
 ) -> HttpGatewayResult<Option<VerificationInfo>> {
-    match (allow_skip_verification, has_ic_certificate(&response)) {
+    if allow_skip_verification {
         // TODO: Remove this (FOLLOW-483)
         // Canisters don't have to provide certified variables
         // This should change in the future, grandfathering in current implementations
-        (true, false) => Ok(None),
-        (_, _) => {
-            let ic_public_key = agent.read_root_key();
-            let verification_info = verify_request_response_pair(
-                request,
-                response,
-                canister_id.as_slice(),
-                get_current_time_in_ns(),
-                MAX_CERT_TIME_OFFSET_NS,
-                ic_public_key.as_slice(),
-                MIN_VERIFICATION_VERSION,
-            )?;
-            Ok(Some(verification_info))
-        }
-    }
-}
-
-fn has_ic_certificate(response: &HttpResponse) -> bool {
-    for (header_name, _) in &response.headers {
-        if header_name.eq_ignore_ascii_case(IC_CERTIFICATE_HEADER_NAME) {
-            return true;
-        }
+        return Ok(None);
     }
 
-    false
+    let ic_public_key = agent.read_root_key();
+    let verification_info = verify_request_response_pair(
+        request,
+        response,
+        canister_id.as_slice(),
+        get_current_time_in_ns(),
+        MAX_CERT_TIME_OFFSET_NS,
+        ic_public_key.as_slice(),
+        MIN_VERIFICATION_VERSION,
+    )?;
+    Ok(Some(verification_info))
 }
 
 fn get_current_time_in_ns() -> u128 {
