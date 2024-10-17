86dcecaf67c474a063dc70d903593f7c9c79210d
Raymond Khalife <raymond.khalife@dfinity.org>
2024-10-16 20:57:09 -0400

    remove unnecessary comments

--- packages/ic-http-gateway/test-container/app.py
+++ packages/ic-http-gateway/test-container/app.py
@@ -11,10 +11,6 @@ def healthcheck():
 
 @app.route('/<path:any_path>', methods=['POST'])
 def handle_post(any_path):
-    # Get the raw data from the POST request
-    # Or you can use `request.form` for form-encoded data
-
-    # Respond to the client
     return "You're making too many requests", 429
 
 if __name__ == '__main__':
